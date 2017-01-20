c3be8b35286a397e6fd6b42f33e5f969355bb564
Oleg Shishkin <olegatormvc@gmail.com>
2017-01-20 17:09:22 +0600

    first commit

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
